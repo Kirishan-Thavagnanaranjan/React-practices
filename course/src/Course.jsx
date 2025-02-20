e38d07aad3d0cebafbc4e1fe99aa4b2610f9b7bc
Kirishan-Thavagnanaranjan <Thavagnanaranjankirishan@gmail.com>
2025-02-21 01:39:43 +0530

    first commit

---
+++ course/src/Course.jsx
@@ -0,0 +1,14 @@
+const course1 = "HTML"
+
+function Course(){
+    return (
+        <div>
+            <img src="" alt=""/>
+            <h3>{course1} </h3>
+            <p> course1 description</p> 
+            <button> Enroll</button>
+        </div>
+    )
+}
+
+export default Course;
---
+++ course/src/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <h2>Code IO</h2>
+            <button>Log in</button>
+        </nav>
+    );
+
+    
+}
+
+export default Navbar;
