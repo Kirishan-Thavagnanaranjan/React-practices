2affe85dbc84ec9234051e33ce61503aec1125ec
Kirishan-Thavagnanaranjan <Thavagnanaranjankirishan@gmail.com>
2025-02-21 04:15:03 +0530

    Added HTML.png asset

--- course/src/Course.jsx
+++ course/src/Course.jsx
@@ -1,13 +1,13 @@
 import HTML from "./assets/HTML.png"
 const course1 = "HTML"
 
-function Course(){
+function Course(props){
     return (
 
         <div className="card">
-            <img src={HTML} alt=""/>
-            <h3>{course1} </h3>
-            <p> This is a full HTML course </p> 
+            <img src={props.image} alt=""/>
+            <h3>{props.name} </h3>
+            <p> {props.price} </p> 
             <button> Enroll</button>
         </div>
     )
