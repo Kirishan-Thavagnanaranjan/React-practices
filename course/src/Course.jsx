79e89dad120a0b722d4f9c039bec55ce78434b8a
Kirishan-Thavagnanaranjan <Thavagnanaranjankirishan@gmail.com>
2025-02-26 23:10:51 +0530

    Add sorting and filtering funnction

--- course/src/Course.jsx
+++ course/src/Course.jsx
@@ -6,10 +6,10 @@ function Course(props){
 
         return (
 
-            <div className="card">
+           props.name && <div className="card">
                 <img src={props.image} alt=""/>
                 <h3>{props.name} </h3>
-                <p> {props.price} </p> 
+                <p> ${props.price} </p> 
                 <span>{props.rating}</span><br/>
                 <button> Enroll</button>
             </div>
--- course/src/CourseList.jsx
+++ course/src/CourseList.jsx
@@ -8,34 +8,41 @@ function CourseList () {
     const courses= [
         {
             name: "HTML Full Course",
-            price: "$499",
+            price: 499,
             image: html,
             rating: 5,
             show: true
         }
         ,{
             name: "CSS Full Course",
-            price: "$499",
+            price: 199,
             image: css,
             rating: 5
         },
         {
             name: "JavaScript",
-            price: "$499",
+            price: 499,
             image: js,
             rating: 5
         },
         {
             name: "React Full Course",
-            price: "$499",
+            price: 299,
             image: react,
             rating: 5
         }
     ];
 
-    const coursesList = courses.map(
-        (course) =>
-        <Course name={course.name}
+    // Sorting function
+
+    courses.sort((x,y) => y.price - x.price) // Desending order
+
+    const vfmCourses = courses.filter((courses) => courses.price>100) // filtering course price greater than 100
+
+    const coursesList = vfmCourses.map(
+        (course,index) =>
+        <Course key={index}
+        name={course.name}
         image={course.image}
         price={course.price}
         rating = {course.rating} />
